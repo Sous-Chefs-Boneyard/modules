fe308e7857a7bed42ffa7a63499a87215e1c6769
Guilhem Lettron <guilhem@lettron.fr>
2012-04-23 21:08:06 +0300

    Update metadata.rb with good entries

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-maintainer       "Fewbytes Technologies"
-maintainer_email "avishai@fewbytes.com"
+maintainer       "Guilhem Lettron"
+maintainer_email "guilhem.lettron@youscribe.com"
 license          "Apache v2.0"
-description      "Configures sysctl parameters"
+description      "Manage modules on linux"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1.0"
 supports         "ubuntu"
