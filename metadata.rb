c61b338331f2d502f75d613768fbf18c2b4e34fd
Guilhem Lettron <guilhem@lettron.fr>
2012-11-14 19:35:54 +0100

    increment version

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "guilhem.lettron@youscribe.com"
 license          "Apache v2.0"
 description      "Manage modules on linux"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
 supports         "ubuntu"
 supports         "debian"
