8a6fcf6337a7380b8894c1f4020ac4cfa097a79e
Lance Albertson <lance@osuosl.org>
2015-04-29 14:30:22 -0700

    Bump to 0.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'guilhem.lettron@youscribe.com'
 license          'Apache v2.0'
 description      'Manage modules on linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 supports         'ubuntu'
 supports         'centos', '~> 6.0'
